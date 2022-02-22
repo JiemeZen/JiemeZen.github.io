70b9c09c68e65830180bfab94cc7229272540aef
Jimmy <jimmychan06@gmail.com>
2022-02-22 21:44:00 +0800

    darkMode enabled
    Implemented dark-mode using localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*
-	Massively by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Jimmy Zeng, 31 Jan 2022
+	Jimmy Zeng, 10 Feb 2022 - Timeline
+	Jimmy Zeng, 22 Feb 2022 - DarkMode
 */
 (function($) {
 
@@ -272,7 +272,42 @@ function toggleTimeline() {
   	}
 }
 
-function darkMode() {
-	var element = document.body;
-	element.classList.toggle("dark-mode");
+
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode'); 
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('dark-mode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('dark-mode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off  
+  } else {  
+    disableDarkMode(); 
   }
+});
