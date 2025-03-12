5c09069e9cd7bd4f13b289bd3b91a78200448e01
Jimmy Zeng <jimmychan06@gmail.com>
2025-03-12 22:01:20 +0800

    articles loaded thru JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -317,3 +317,17 @@ function myFunction(x) {
 	x.classList.toggle("fa-sun");
 	console.log("pressed")
 }
+
+function loadHTML(url, elementId) {
+	fetch(url)
+		.then(response => response.text())
+		.then(data => {
+			document.getElementById(elementId).innerHTML = data;
+		})
+		.catch(error => console.error('Error loading HTML:', error));
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	loadHTML('social.html', 'social-section');
+	loadHTML('sitemap.html', 'sitemap-section');
+});
